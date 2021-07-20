5b37a9f52cd092583737c948ef33f6a9217d58a1
Brian Smith <brian.bubbam@gmail.com>
2021-07-20 08:51:39 -0500

    commit tree f962ea5f35ca1845aa3b3cacb8f2a5e6c77bab9a

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+
+class Dog
+
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        self.save # calls the save method on initialization
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+
+    def self.clear_all
+        @@all.clear # this will clear out the @@all array
+    end
+
+
+end
